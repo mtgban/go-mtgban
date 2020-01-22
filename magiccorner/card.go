c5dc9d3f1887cdb83d3175e1d71a5c75463b96be
Vittorio Giovara <vittorio.giovara@gmail.com>
2020-01-22 10:17:38 -0500

    Rewrite magiccorner module

--- magiccorner/card.go
+++ magiccorner/card.go
@@ -2,20 +2,14 @@ package magiccorner
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/kodabb/go-mtgban/mtgban"
-	"github.com/kodabb/go-mtgban/mtgjson"
 )
 
 type MCCard struct {
 	Name string `json:"name"`
 	Set  string `json:"set"`
 	Foil bool   `json:"foil"`
-
-	Pricing   float64 `json:"price"`
-	Qty       int     `json:"quantity"`
-	Condition string  `json:"conditions"`
 
 	Id     int    `json:"id"`
 	Number string `json:"number"`
@@ -27,7 +21,6 @@ type MCCard struct {
 
 var cardTable = map[string]string{
 	"Ambitions's Cost":        "Ambition's Cost",
-	"Elspeth, Knight Errant":  "Elspeth, Knight-Errant",
 	"Fire (Fire/Ice) ":        "Fire",
 	"Fire (Fire/Ice)":         "Fire",
 	"Fire/Ice":                "Fire",
@@ -37,48 +30,22 @@ var cardTable = map[string]string{
 	"Time Shrike":             "Tine Shrike",
 	"Treasure Find":           "Treasured Find",
 	"Who/What/When/Where/Why": "Who",
+	"Who,What,When,Where,Why": "Who",
+
+	"Elspeth, Knight Errant (Mythic Edition)": "Elspeth, Knight-Errant",
 }
 
-func (c *MCCard) CanonicalCard(db mtgjson.MTGDB) (*mtgban.Card, error) {
-	variants := mtgban.SplitVariants(c.Name)
+func (mc *Magiccorner) Convert(c *MCCard) (*mtgban.Card, error) {
+	setName, setCheck := mc.parseSet(c)
+	cardName, numberCheck := mc.parseNumber(c, setName)
 
-	specifier := ""
-	if len(variants) > 1 {
-		specifier = variants[1]
-	}
-	cname := variants[0]
-
-	lutName, found := cardTable[cname]
-	if found {
-		cname = lutName
-	}
-
-	newName, setName, setCheck := c.parseSet(db, cname, specifier)
-	cardName, number, numberCheck := c.parseNumber(newName, setName, specifier)
-
-	variants = mtgban.SplitVariants(cardName)
-	cardName = variants[0]
-
-	// Only keep one of the split cards
-	switch {
-	// Only keep one of the split/ transform cards
-	case strings.Contains(cardName, " // "):
-		cn := strings.Split(cardName, " // ")
-		cardName = cn[0]
-	// Only keep one of the transform cards
-	case strings.Contains(cardName, " / "):
-		cn := strings.Split(cardName, " / ")
-		cardName = cn[0]
-	}
-
-	n := mtgban.NewNormalizer()
-	cardName = n.Normalize(cardName)
+	cardName = mc.norm.Normalize(cardName)
 
 	// Loop over the DB
-	for _, set := range db {
+	for _, set := range mc.db {
 		if setCheck(set) {
 			for _, card := range set.Cards {
-				dbCardName := n.Normalize(card.Name)
+				dbCardName := mc.norm.Normalize(card.Name)
 
 				// These sets sometimes have extra stuff that we stripped earlier
 				if set.Type == "funny" {
@@ -89,9 +56,9 @@ func (c *MCCard) CanonicalCard(db mtgjson.MTGDB) (*mtgban.Card, error) {
 				// Check card name
 				cardCheck := dbCardName == cardName
 
-				// If card number is available, use it to narrow results
+				// Narrow results with the number callback
 				if numberCheck != nil {
-					cardCheck = cardCheck && numberCheck(set, card.Number)
+					cardCheck = cardCheck && numberCheck(set, card)
 				}
 
 				if cardCheck {
@@ -110,25 +77,5 @@ func (c *MCCard) CanonicalCard(db mtgjson.MTGDB) (*mtgban.Card, error) {
 		}
 	}
 
-	return nil, fmt.Errorf("Card not found '%s->%s' in '%s->%s' (foil=%v) {%s} [%v]", c.Name, cardName, c.Set, setName, c.Foil, number, c)
-}
-
-func (c *MCCard) Conditions() string {
-	cond := c.Condition
-	if cond == "NM/M" {
-		cond = "NM"
-	}
-	return cond
-}
-func (c *MCCard) Market() string {
-	return "Magic Corner"
-}
-func (c *MCCard) Price() float64 {
-	return c.Pricing
-}
-func (c *MCCard) TradePrice() float64 {
-	return 0
-}
-func (c *MCCard) Quantity() int {
-	return c.Qty
+	return nil, fmt.Errorf("Card not found '%s->%s' in '%s->%s' (foil=%v) [%v]", c.Name, cardName, c.Set, setName, c.Foil, c)
 }
