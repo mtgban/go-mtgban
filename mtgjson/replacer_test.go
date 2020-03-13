5998233e1a282cec2224717684b4e2e7b79d2696
Vittorio Giovara <vittorio.giovara@gmail.com>
2020-03-12 22:49:35 -0400

    mtgjson: Provide a new Replacer and Normalize API
    It is stricter, has more rules, and it is simpler to use than mtgban's.

---
+++ mtgjson/replacer_test.go
@@ -0,0 +1,78 @@
+package mtgjson
+
+import "testing"
+
+type NormalizeTest struct {
+	In  string
+	Out string
+}
+
+var NormalizeTests = []NormalizeTest{
+	NormalizeTest{
+		In:  "Lotus Bloom",
+		Out: "lotubloom",
+	},
+	NormalizeTest{
+		In:  "Lotus Blossom",
+		Out: "lotublossom",
+	},
+	NormalizeTest{
+		In:  "Tangle Asp",
+		Out: "tangleasp",
+	},
+	NormalizeTest{
+		In:  "Tanglesap",
+		Out: "tangleap",
+	},
+	NormalizeTest{
+		In:  "Ghazbán Ogre",
+		Out: "ghazbanogre",
+	},
+	NormalizeTest{
+		In:  "Ghazban Ogress",
+		Out: "ghazbanogress",
+	},
+	NormalizeTest{
+		In:  "Flame Slash",
+		Out: "flameslash",
+	},
+	NormalizeTest{
+		In:  "Flame Lash",
+		Out: "flamelah",
+	},
+	NormalizeTest{
+		In:  "Waste Land",
+		Out: "waste land",
+	},
+	NormalizeTest{
+		In:  "Wasteland",
+		Out: "wateland",
+	},
+	NormalizeTest{
+		In:  "  spaaaaaaace  ",
+		Out: "paaaaaaace",
+	},
+	NormalizeTest{
+		In:  "Ætherflux reservoir",
+		Out: "aetherfluxreervoir",
+	},
+	NormalizeTest{
+		In:  "forest b",
+		Out: "foretb",
+	},
+}
+
+func TestNormalize(t *testing.T) {
+	for _, probe := range NormalizeTests {
+		test := probe
+		t.Run(test.In, func(t *testing.T) {
+			t.Parallel()
+			out := Normalize(test.In)
+			if out != test.Out {
+				t.Errorf("FAIL %s: Expected '%s' got '%s'", test.In, test.Out, out)
+				return
+			}
+			t.Log("PASS:", test.In)
+		})
+	}
+}
