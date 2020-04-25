40866d34259b8d2a2db15f2ef3cb7688e80065fa
Vittorio Giovara <vittorio.giovara@gmail.com>
2020-04-24 23:06:23 -0400

    abugames: Adjust conditions conversion based on the actual scale

--- abugames/abugames.go
+++ abugames/abugames.go
@@ -56,6 +56,14 @@ func (abu *ABUGames) processEntry(page int) (res resultChan) {
 	duplicate := map[string]bool{}
 
 	for _, group := range product.Grouped.ProductId.Groups {
+		// Use a different grading scale if MINT is present
+		hasMintSeparate := false
+		for _, card := range group.Doclist.Cards {
+			if card.Condition == "MINT" {
+				hasMintSeparate = true
+				break
+			}
+		}
 		for _, card := range group.Doclist.Cards {
 			// Deprecated value
 			if card.Condition == "SP" {
@@ -64,11 +72,23 @@ func (abu *ABUGames) processEntry(page int) (res resultChan) {
 
 			cond := card.Condition
 			switch cond {
-			case "NM", "HP":
 			case "MINT":
 				cond = "NM"
+			case "NM":
+				cond = "NM"
+				if hasMintSeparate {
+					cond = "SP"
+				}
 			case "PLD":
 				cond = "SP"
+				if hasMintSeparate {
+					cond = "MP"
+				}
+			case "HP":
+				cond = "MP"
+				if hasMintSeparate {
+					cond = "HP"
+				}
 			default:
 				abu.printf("Unknown '%s' condition", cond)
 				continue
