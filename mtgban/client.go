08053bac908abe5a2adb7c05b59ed80fa04a42f3
Vittorio Giovara <vittorio.giovara@gmail.com>
2020-05-15 22:51:39 -0400

    mtgban/client: Allow to disable portions of the registered scrapers

--- mtgban/client.go
+++ mtgban/client.go
@@ -8,13 +8,17 @@ import (
 // Scraper type types, as well as offering a way to retrieve a single or
 // multiple Scapers.
 type BanClient struct {
-	scrapers map[string]Scraper
+	scrapers       map[string]Scraper
+	sellerDisabled map[string]bool
+	vendorDisabled map[string]bool
 }
 
 // Return an empty BanClient
 func NewClient() *BanClient {
 	bc := BanClient{}
 	bc.scrapers = map[string]Scraper{}
+	bc.sellerDisabled = map[string]bool{}
+	bc.vendorDisabled = map[string]bool{}
 	return &bc
 }
 
@@ -23,18 +27,30 @@ func (bc *BanClient) Register(scraper Scraper) {
 	bc.scrapers[scraper.Info().Shorthand] = scraper
 }
 
+// Add a Scraper to the client, enable the seller side only (if any)
+func (bc *BanClient) RegisterSeller(scraper Scraper) {
+	bc.scrapers[scraper.Info().Shorthand] = scraper
+	bc.vendorDisabled[scraper.Info().Shorthand] = true
+}
+
+// Add a Scraper to the client, enable the vendor side only (if any)
+func (bc *BanClient) RegisterVendor(scraper Scraper) {
+	bc.scrapers[scraper.Info().Shorthand] = scraper
+	bc.sellerDisabled[scraper.Info().Shorthand] = true
+}
+
 // Load inventory and buylist content for each scraper registered in the client
 func (bc *BanClient) Load() error {
 	for _, scraper := range bc.scrapers {
 		seller, ok := scraper.(Seller)
-		if ok {
+		if ok && !bc.sellerDisabled[scraper.Info().Shorthand] {
 			_, err := seller.Inventory()
 			if err != nil {
 				return err
 			}
 		}
 		vendor, ok := scraper.(Vendor)
-		if ok {
+		if ok && !bc.vendorDisabled[scraper.Info().Shorthand] {
 			_, err := vendor.Buylist()
 			if err != nil {
 				return err
@@ -65,7 +81,7 @@ func (bc *BanClient) Scrapers() (scrapers []Scraper) {
 func (bc *BanClient) Sellers() (sellers []Seller) {
 	for _, maybeSeller := range bc.scrapers {
 		seller, ok := maybeSeller.(Seller)
-		if !ok {
+		if !ok || bc.sellerDisabled[maybeSeller.Info().Shorthand] {
 			continue
 		}
 		sellers = append(sellers, seller)
@@ -77,7 +93,7 @@ func (bc *BanClient) Sellers() (sellers []Seller) {
 func (bc *BanClient) Vendors() (vendors []Vendor) {
 	for _, maybeVendor := range bc.scrapers {
 		vendor, ok := maybeVendor.(Vendor)
-		if !ok {
+		if !ok || bc.vendorDisabled[maybeVendor.Info().Shorthand] {
 			continue
 		}
 		vendors = append(vendors, vendor)
@@ -89,7 +105,7 @@ func (bc *BanClient) Vendors() (vendors []Vendor) {
 func (bc *BanClient) Markets() (markets []Market) {
 	for _, maybeMarket := range bc.scrapers {
 		market, ok := maybeMarket.(Market)
-		if !ok {
+		if !ok || bc.sellerDisabled[maybeMarket.Info().Shorthand] {
 			continue
 		}
 		markets = append(markets, market)
