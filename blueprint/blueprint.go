e0792945ae7fb3d9edf97db8d4811cce15357afc
Vittorio Giovara <vittorio.giovara@gmail.com>
2020-09-20 17:08:28 +0200

    Add blueprint module

---
+++ blueprint/blueprint.go
@@ -0,0 +1,157 @@
+package blueprint
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	excelize "github.com/360EntSecGroup-Skylar/excelize/v2"
+
+	"github.com/kodabb/go-mtgban/mtgban"
+	"github.com/kodabb/go-mtgban/mtgmatcher"
+)
+
+const (
+	BlueprintURL = "http://www.mtgblueprint.com/s/Blueprint_%s.xlsx"
+)
+
+type Blueprint struct {
+	LogCallback mtgban.LogCallbackFunc
+	buylistDate time.Time
+
+	buylist mtgban.BuylistRecord
+}
+
+func NewScraper() *Blueprint {
+	bp := Blueprint{}
+	bp.buylist = mtgban.BuylistRecord{}
+	return &bp
+}
+
+func (bp *Blueprint) printf(format string, a ...interface{}) {
+	if bp.LogCallback != nil {
+		bp.LogCallback("[BP] "+format, a...)
+	}
+}
+
+func (bp *Blueprint) parseBL() error {
+	var reader io.ReadCloser
+	for i := 0; i < 12; i++ {
+		t := time.Now().AddDate(0, -i, 0)
+		blURL := fmt.Sprintf(BlueprintURL, t.Format("January_2006"))
+		bp.printf("Trying %s", blURL)
+		resp, err := http.Get(blURL)
+		if err == nil && resp.StatusCode == 200 {
+			reader = resp.Body
+			defer resp.Body.Close()
+			break
+		}
+		resp.Body.Close()
+		bp.printf("not found, continuing")
+	}
+	if reader == nil {
+		bp.printf("no updates over a year")
+		return nil
+	}
+
+	f, err := excelize.OpenReader(reader)
+	if err != nil {
+		return err
+	}
+
+	sheets := f.GetSheetList()
+	var i int
+	for i = range sheets {
+		if sheets[i] != "Instructions" {
+			break
+		}
+	}
+
+	// Get all the rows in the Sheet1.
+	rows, err := f.GetRows(sheets[i])
+	for _, row := range rows {
+		if len(row) < 5 {
+			continue
+		}
+		cardName := row[4]
+		edition := row[5]
+		price, _ := strconv.ParseFloat(row[2], 64)
+
+		if cardName == "" || edition == "" || price == 0 {
+			continue
+		}
+
+		variant := ""
+		if strings.Contains(cardName, "(") {
+			vars := mtgmatcher.SplitVariants(cardName)
+			cardName = vars[0]
+			if len(vars) > 1 {
+				variant = vars[1]
+			}
+		}
+
+		theCard := &mtgmatcher.Card{
+			Name:      cardName,
+			Edition:   edition,
+			Variation: variant,
+		}
+
+		cardId, err := mtgmatcher.Match(theCard)
+		if err != nil {
+			bp.printf("%v", err)
+			bp.printf("%q", theCard)
+			bp.printf("%q", row)
+			alias, ok := err.(*mtgmatcher.AliasingError)
+			if ok {
+				probes := alias.Probe()
+				for _, probe := range probes {
+					card, _ := mtgmatcher.Unmatch(probe)
+					bp.printf("- %s", card)
+				}
+			}
+			continue
+		}
+
+		out := &mtgban.BuylistEntry{
+			BuyPrice: price,
+			URL:      "http://www.mtgblueprint.com/",
+		}
+		err = bp.buylist.Add(cardId, out)
+		if err != nil {
+			bp.printf("%v", err)
+		}
+	}
+
+	bp.buylistDate = time.Now()
+
+	return nil
+}
+
+func (bp *Blueprint) Buylist() (mtgban.BuylistRecord, error) {
+	if len(bp.buylist) > 0 {
+		return bp.buylist, nil
+	}
+
+	err := bp.parseBL()
+	if err != nil {
+		return nil, err
+	}
+
+	return bp.buylist, nil
+}
+
+func grading(_ string, entry mtgban.BuylistEntry) (grade map[string]float64) {
+	return nil
+}
+
+func (bp *Blueprint) Info() (info mtgban.ScraperInfo) {
+	info.Name = "Blueprint"
+	info.Shorthand = "BP"
+	info.BuylistTimestamp = bp.buylistDate
+	info.Grading = grading
+	info.NoCredit = true
+	return
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/kodabb/go-mtgban
 go 1.14
 
 require (
+	github.com/360EntSecGroup-Skylar/excelize/v2 v2.3.0
 	github.com/PuerkitoBio/goquery v1.5.0
 	github.com/antchfx/htmlquery v1.2.0 // indirect
 	github.com/antchfx/xmlquery v1.2.1 // indirect
@@ -12,6 +13,5 @@ require (
 	github.com/golang/groupcache v0.0.0-20191027212112-611e8accdfc9 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.1
 	github.com/hashicorp/go-retryablehttp v0.6.6
-	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
 	google.golang.org/appengine v1.6.5 // indirect
 )
