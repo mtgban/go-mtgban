fc0f316eff0441b8379ca7bad1005594d348b119
Vittorio Giovara <vittorio.giovara@gmail.com>
2020-10-16 05:50:32 +0200

    tcgplayer: Move authorization code to the api file

--- tcgplayer/api.go
+++ tcgplayer/api.go
@@ -4,18 +4,36 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"net/url"
 	"strings"
+	"time"
 
-	http "github.com/hashicorp/go-retryablehttp"
+	retryablehttp "github.com/hashicorp/go-retryablehttp"
+)
+
+const (
+	defaultConcurrency = 8
+	defaultAPIRetry    = 5
+
+	pagesPerRequest = 50
+	tcgBaseURL      = "https://shop.tcgplayer.com/productcatalog/product/getpricetable?productId=0&gameName=magic&useV2Listings=true&page=0&pageSize=0&sortValue=price"
+
+	tcgApiVersion    = "v1.39.0"
+	tcgApiProductURL = "https://api.tcgplayer.com/" + tcgApiVersion + "/pricing/product/"
+	tcgApiPricingURL = "https://api.tcgplayer.com/" + tcgApiVersion + "/pricing/sku/"
+	tcgApiBuylistURL = "https://api.tcgplayer.com/" + tcgApiVersion + "/pricing/buy/product/"
+	tcgApiSKUURL     = "https://api.tcgplayer.com/" + tcgApiVersion + "/catalog/products/%s/skus"
+	tcgApiSearchURL  = "https://api.tcgplayer.com/" + tcgApiVersion + "/catalog/categories/1/search"
 )
 
 type TCGClient struct {
-	client *http.Client
+	client *retryablehttp.Client
 }
 
 func NewTCGClient(publicId, privateId string) *TCGClient {
 	tcg := TCGClient{}
-	tcg.client = http.NewClient()
+	tcg.client = retryablehttp.NewClient()
 	tcg.client.Logger = nil
 	tcg.client.HTTPClient.Transport = &authTransport{
 		Parent:    tcg.client.HTTPClient.Transport,
@@ -23,6 +41,53 @@ func NewTCGClient(publicId, privateId string) *TCGClient {
 		PrivateId: privateId,
 	}
 	return &tcg
+}
+
+type authTransport struct {
+	Parent    http.RoundTripper
+	PublicId  string
+	PrivateId string
+	token     string
+	expires   time.Time
+}
+
+func (t *authTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	if t.token == "" || t.expires.After(time.Now()) {
+		if t.PublicId == "" || t.PrivateId == "" {
+			return nil, fmt.Errorf("missing public or private id")
+		}
+		params := url.Values{}
+		params.Set("grant_type", "client_credentials")
+		params.Set("client_id", t.PublicId)
+		params.Set("client_secret", t.PrivateId)
+		body := strings.NewReader(params.Encode())
+
+		resp, err := retryablehttp.Post("https://api.tcgplayer.com/token", "application/json", body)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		var response struct {
+			AccessToken string        `json:"access_token"`
+			Expires     time.Duration `json:"expires"`
+		}
+		err = json.Unmarshal(data, &response)
+		if err != nil {
+			return nil, err
+		}
+
+		t.token = response.AccessToken
+		t.expires = time.Now().Add(response.Expires)
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", t.token))
+	return t.Parent.RoundTrip(req)
 }
 
 type TCGPrice struct {
--- tcgplayer/tcgplayer.go
+++ tcgplayer/tcgplayer.go
@@ -1,35 +1,16 @@
 package tcgplayer
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/url"
 	"strconv"
 	"strings"
-	"time"
 
 	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
-	cleanhttp "github.com/hashicorp/go-cleanhttp"
 
 	"github.com/kodabb/go-mtgban/mtgban"
-)
-
-const (
-	defaultConcurrency = 8
-	defaultAPIRetry    = 5
-
-	pagesPerRequest = 50
-	tcgBaseURL      = "https://shop.tcgplayer.com/productcatalog/product/getpricetable?productId=0&gameName=magic&useV2Listings=true&page=0&pageSize=0&sortValue=price"
-
-	tcgApiVersion    = "v1.39.0"
-	tcgApiProductURL = "https://api.tcgplayer.com/" + tcgApiVersion + "/pricing/product/"
-	tcgApiPricingURL = "https://api.tcgplayer.com/" + tcgApiVersion + "/pricing/sku/"
-	tcgApiBuylistURL = "https://api.tcgplayer.com/" + tcgApiVersion + "/pricing/buy/product/"
-	tcgApiSKUURL     = "https://api.tcgplayer.com/" + tcgApiVersion + "/catalog/products/%s/skus"
-	tcgApiSearchURL  = "https://api.tcgplayer.com/" + tcgApiVersion + "/catalog/categories/1/search"
 )
 
 type requestChan struct {
@@ -74,58 +55,4 @@ func getListingsNumber(client *http.Client, productId string) (int, error) {
 	}
 
 	return entriesNum, nil
-}
-
-type authTransport struct {
-	Parent    http.RoundTripper
-	PublicId  string
-	PrivateId string
-	token     string
-	expires   time.Time
-}
-
-func (t *authTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	if t.token == "" || t.expires.After(time.Now()) {
-		if t.PublicId == "" || t.PrivateId == "" {
-			return nil, fmt.Errorf("missing public or private id")
-		}
-		params := url.Values{}
-		params.Set("grant_type", "client_credentials")
-		params.Set("client_id", t.PublicId)
-		params.Set("client_secret", t.PrivateId)
-		body := strings.NewReader(params.Encode())
-
-		r, err := http.NewRequest("POST", "https://api.tcgplayer.com/token", body)
-		if err != nil {
-			return nil, err
-		}
-		r.Header.Add("Content-Type", "application/json")
-
-		client := cleanhttp.DefaultClient()
-		resp, err := client.Do(r)
-		if err != nil {
-			return nil, err
-		}
-		defer resp.Body.Close()
-
-		data, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return nil, err
-		}
-
-		var response struct {
-			AccessToken string        `json:"access_token"`
-			Expires     time.Duration `json:"expires"`
-		}
-		err = json.Unmarshal(data, &response)
-		if err != nil {
-			return nil, err
-		}
-
-		t.token = response.AccessToken
-		t.expires = time.Now().Add(response.Expires)
-	}
-
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", t.token))
-	return t.Parent.RoundTrip(req)
 }
