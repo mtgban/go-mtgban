756e86db068d4b777487f6f2d4ae427fca25cda8
Vittorio Giovara <vittorio.giovara@gmail.com>
2020-12-04 01:18:10 +0100

    cardmarket: Move filtered expansions and cards out of a function
    So that the lists may be reused elsewhere.

--- cardmarket/utils.go
+++ cardmarket/utils.go
@@ -12,6 +12,106 @@ import (
 	"github.com/kodabb/go-mtgban/mtgmatcher"
 )
 
+var filteredExpansions = []string{
+	"73",   // Foreign White Bordered
+	"96",   // Rinascimento
+	"105",  // Salvat-Hachette
+	"110",  // Oversized 6x9 Promos
+	"111",  // Oversized Box Toppers
+	"1259", // Salvat-Hachette 2011
+	"1269", // 2006 Player Cards
+	"1281", // Your Move Games Tokens
+	"1332", // Fourth Edition: Black Bordered
+	"1391", // 2005 Player Cards
+	"1392", // 2007 Player Card
+	"1401", // Simplified Chinese Alternate Art Cards
+	"1408", // Filler Cards
+	"1451", // Ultra-Pro Puzzle Cards
+	"1500", // Starcity Games: Commemorative Tokens
+	"1502", // Tokens for MTG
+	"1599", // Chronicles: Japanese
+	"1600", // Fourth Edition: Alternate
+	"1638", // Fallen Empires: Wyvern Misprints
+	"1639", // Oversized 9x12 Promos
+	"1659", // Yummy Tokens
+	"1691", // GnD Cards
+	"1705", // Javi Alterations Tokens
+	"1833", // Rk post Products
+	"1834", // Mezzocielo & Friends Classic Tokens
+	"1837", // The Dark Italian
+	"1838", // Legends Italian
+	"2114", // Johannes Voss Tokens
+	"2484", // Modern Horizons: Art Series
+	"2572", // Classic Art Tokens
+	"2529", // Cats & Cantrips Tokens
+	"2567", // Tokens of Spirit
+}
+
+var filteredCards = []string{
+	"Build a Deck: The Basics // Popular Magic Formats",
+
+	// TFTH tokens
+	"Hydra Head",
+	"Ravenous Brute Head",
+	"Savage Vigor Head",
+	"Shrieking Titan Head",
+	"Snapping Fang Head",
+	"Disorienting Glower",
+	"Distract the Hydra",
+	"Grown from the Stump",
+	"Hydra's Impenetrable Hide",
+	"Noxious Hydra Breath",
+	"Neck Tangle",
+	"Strike the Weak Spot",
+	"Torn Between Heads",
+	"Swallow the Hero Whole",
+	"Unified Lunge",
+
+	// TBTH tokens
+	"Minotaur Goreseeker",
+	"Minotaur Younghorn",
+	"Mogis's Chosen",
+	"Phoberos Reaver",
+	"Reckless Minotaur",
+	"Altar of Mogis",
+	"Consuming Rage",
+	"Intervention of Keranos",
+	"Descend on the Prey",
+	"Plundered Statue",
+	"Refreshing Elixir",
+	"Touch of the Horned God",
+	"Massacre Totem",
+	"Unquenchable Fury",
+	"Vitality Salve",
+
+	// TDAG tokens
+	"Xenagos Ascended",
+	"Rollicking Throng",
+	"Ecstatic Piper",
+	"Maddened Oread",
+	"Pheres-Band Revelers",
+	"Serpent Dancers",
+	"Wild Maenads",
+	"Dance of Flame",
+	"Dance of Panic",
+	"Impulsive Destruction",
+	"Impulsive Charge",
+	"Impulsive Return",
+	"Rip to Pieces",
+	"Xenagos's Strike",
+	"Xenagos's Scorn",
+
+	// Unique
+	"1996 World Champion",
+	"Shichifukujin Dragon",
+	"Proposal",
+	"Magic Guru",
+	"Fraternal Exaltation",
+	"Robot Chicken",
+	"Phoenix Heart",
+	"Splendid Genesis",
+}
+
 type MKMProductIdPair struct {
 	ProductId   string
 	ExpansionId string
@@ -66,111 +166,28 @@ func (mkm *MKMClient) ListProductIds() ([]MKMProductIdPair, error) {
 		}
 
 		// Skip unsupported sets
-		switch expansionId {
-		case "73", // Foreign White Bordered
-			"96",   // Rinascimento
-			"105",  // Salvat-Hachette
-			"110",  // Oversized 6x9 Promos
-			"111",  // Oversized Box Toppers
-			"1259", // Salvat-Hachette 2011
-			"1269", // 2006 Player Cards
-			"1281", // Your Move Games Tokens
-			"1332", // Fourth Edition: Black Bordered
-			"1391", // 2005 Player Cards
-			"1392", // 2007 Player Card
-			"1401", // Simplified Chinese Alternate Art Cards
-			"1408", // Filler Cards
-			"1451", // Ultra-Pro Puzzle Cards
-			"1500", // Starcity Games: Commemorative Tokens
-			"1502", // Tokens for MTG
-			"1599", // Chronicles: Japanese
-			"1600", // Fourth Edition: Alternate
-			"1638", // Fallen Empires: Wyvern Misprints
-			"1639", // Oversized 9x12 Promos
-			"1659", // Yummy Tokens
-			"1691", // GnD Cards
-			"1705", // Javi Alterations Tokens
-			"1833", // Rk post Products
-			"1834", // Mezzocielo & Friends Classic Tokens
-			"1837", // The Dark Italian
-			"1838", // Legends Italian
-			"2114", // Johannes Voss Tokens
-			"2484", // Modern Horizons: Art Series
-			"2572", // Classic Art Tokens
-			"2529", // Cats & Cantrips Tokens
-			"2567", // Tokens of Spirit
-			"":
+		skipExpansion := false
+		for _, expId := range filteredExpansions {
+			if expansionId == expId {
+				skipExpansion = true
+				break
+			}
+		}
+		if skipExpansion {
 			continue
 		}
 
 		// Skip unsupported cards
-		if mtgmatcher.IsToken(cardName) ||
-			strings.Contains(cardName, "On Your Turn") {
-			continue
+		skipCard := false
+		for _, name := range filteredCards {
+			if cardName == name {
+				skipCard = true
+				break
+			}
 		}
-		switch cardName {
-		case "Build a Deck: The Basics // Popular Magic Formats",
-
-			// TFTH tokens
-			"Hydra Head",
-			"Ravenous Brute Head",
-			"Savage Vigor Head",
-			"Shrieking Titan Head",
-			"Snapping Fang Head",
-			"Disorienting Glower",
-			"Distract the Hydra",
-			"Grown from the Stump",
-			"Hydra's Impenetrable Hide",
-			"Noxious Hydra Breath",
-			"Neck Tangle",
-			"Strike the Weak Spot",
-			"Torn Between Heads",
-			"Swallow the Hero Whole",
-			"Unified Lunge",
-
-			// TBTH tokens
-			"Minotaur Goreseeker",
-			"Minotaur Younghorn",
-			"Mogis's Chosen",
-			"Phoberos Reaver",
-			"Reckless Minotaur",
-			"Altar of Mogis",
-			"Consuming Rage",
-			"Intervention of Keranos",
-			"Descend on the Prey",
-			"Plundered Statue",
-			"Refreshing Elixir",
-			"Touch of the Horned God",
-			"Massacre Totem",
-			"Unquenchable Fury",
-			"Vitality Salve",
-
-			// TDAG tokens
-			"Xenagos Ascended",
-			"Rollicking Throng",
-			"Ecstatic Piper",
-			"Maddened Oread",
-			"Pheres-Band Revelers",
-			"Serpent Dancers",
-			"Wild Maenads",
-			"Dance of Flame",
-			"Dance of Panic",
-			"Impulsive Destruction",
-			"Impulsive Charge",
-			"Impulsive Return",
-			"Rip to Pieces",
-			"Xenagos's Strike",
-			"Xenagos's Scorn",
-
-			// Unique
-			"1996 World Champion",
-			"Shichifukujin Dragon",
-			"Proposal",
-			"Magic Guru",
-			"Fraternal Exaltation",
-			"Robot Chicken",
-			"Phoenix Heart",
-			"Splendid Genesis":
+		if skipCard ||
+			mtgmatcher.IsToken(cardName) ||
+			strings.Contains(cardName, "On Your Turn") {
 			continue
 		}
 
