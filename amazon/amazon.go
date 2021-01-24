e4533a61c7488b7bd6326ff932e6314b4587454a
Vittorio Giovara <vittorio.giovara@gmail.com>
2021-01-24 22:31:18 +0100

    Add amazon module

---
+++ amazon/amazon.go
@@ -0,0 +1,186 @@
+package amazon
+
+import (
+	"net/url"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/kodabb/go-mtgban/mtgban"
+	"github.com/kodabb/go-mtgban/mtgmatcher"
+)
+
+const (
+	defaultConcurrency = 4
+)
+
+type Amazon struct {
+	LogCallback    mtgban.LogCallbackFunc
+	MaxConcurrency int
+
+	inventoryDate time.Time
+	inventory     mtgban.InventoryRecord
+
+	client *AMZClient
+}
+
+type respChan struct {
+	cardId string
+	entry  *mtgban.InventoryEntry
+}
+
+func NewScraper(token string) *Amazon {
+	amz := Amazon{}
+	amz.inventory = mtgban.InventoryRecord{}
+	amz.client = NewAMZClient(token)
+	amz.MaxConcurrency = defaultConcurrency
+	return &amz
+}
+
+func (amz *Amazon) printf(format string, a ...interface{}) {
+	if amz.LogCallback != nil {
+		amz.LogCallback("[AMZ] "+format, a...)
+	}
+}
+
+func (amz *Amazon) processUUIDs(channel chan<- respChan, ids []string) error {
+	results, err := amz.client.GetPrices(ids)
+	if err != nil {
+		return err
+	}
+
+	for uuid, result := range results {
+		var cardId string
+		var cardIdFoil string
+
+		for tag, price := range result {
+			if price == 0 {
+				continue
+			}
+
+			if cardId == "" && strings.HasSuffix(tag, "Normal") {
+				cardId, err = mtgmatcher.Match(&mtgmatcher.Card{
+					Id: uuid,
+				})
+				if err != nil {
+					amz.printf("%s - %s", uuid, err)
+					continue
+				}
+			} else if cardIdFoil == "" && strings.HasSuffix(tag, "Foil") {
+				cardIdFoil, err = mtgmatcher.Match(&mtgmatcher.Card{
+					Id:   uuid,
+					Foil: true,
+				})
+				if err != nil {
+					amz.printf("%s - %s", uuid, err)
+					continue
+				}
+			}
+
+			outId := cardId
+			if strings.HasSuffix(tag, "Foil") {
+				outId = cardIdFoil
+			}
+
+			co, err := mtgmatcher.GetUUID(outId)
+			if err != nil {
+				amz.printf("%s - %s", outId, err)
+				continue
+			}
+
+			link := "https://amazon.com/s?k=" + url.QueryEscape(co.Name+" "+co.Edition+" mtg")
+
+			out := respChan{
+				cardId: outId,
+				entry: &mtgban.InventoryEntry{
+					Conditions: strings.ToUpper(tag[:2]),
+					Price:      price,
+					Quantity:   1,
+					URL:        link,
+				},
+			}
+
+			channel <- out
+		}
+	}
+
+	return nil
+}
+
+func (amz *Amazon) scrape() error {
+	pages := make(chan string)
+	channel := make(chan respChan)
+	var wg sync.WaitGroup
+
+	for i := 0; i < amz.MaxConcurrency; i++ {
+		wg.Add(1)
+		go func() {
+			buffer := make([]string, 0, 20)
+			for uuid := range pages {
+				buffer = append(buffer, uuid)
+				if len(buffer) == 20 {
+					err := amz.processUUIDs(channel, buffer)
+					if err != nil {
+						amz.printf("%s", err.Error())
+					}
+					buffer = buffer[:0]
+				}
+			}
+			// Spillover
+			if len(buffer) != 0 {
+				err := amz.processUUIDs(channel, buffer)
+				if err != nil {
+					amz.printf("%s", err.Error())
+				}
+			}
+			wg.Done()
+		}()
+	}
+
+	go func() {
+		sets := mtgmatcher.GetSets()
+		for _, set := range sets {
+			amz.printf("Processing %s", set.Name)
+			for _, card := range set.Cards {
+				pages <- card.UUID
+			}
+		}
+		close(pages)
+
+		wg.Wait()
+		close(channel)
+	}()
+
+	for record := range channel {
+		err := amz.inventory.Add(record.cardId, record.entry)
+		if err != nil {
+			amz.printf("%v", err)
+			continue
+		}
+	}
+
+	amz.inventoryDate = time.Now()
+
+	return nil
+}
+
+func (amz *Amazon) Inventory() (mtgban.InventoryRecord, error) {
+	if len(amz.inventory) > 0 {
+		return amz.inventory, nil
+	}
+
+	err := amz.scrape()
+	if err != nil {
+		return nil, err
+	}
+
+	return amz.inventory, nil
+}
+
+func (amz *Amazon) Info() (info mtgban.ScraperInfo) {
+	info.Name = "Amazon"
+	info.Shorthand = "AMZ"
+	info.InventoryTimestamp = amz.inventoryDate
+	info.NoQuantityInventory = true
+	return
+}
---
+++ amazon/api.go
@@ -0,0 +1,60 @@
+package amazon
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	retryablehttp "github.com/hashicorp/go-retryablehttp"
+)
+
+const (
+	amzAPIURL = "http://greatermossdogapi.us-east-1.elasticbeanstalk.com/api/v1/pricing/"
+)
+
+type AMZClient struct {
+	client *retryablehttp.Client
+}
+
+type authTransport struct {
+	Parent http.RoundTripper
+	Token  string
+}
+
+func NewAMZClient(token string) *AMZClient {
+	amz := AMZClient{}
+	amz.client = retryablehttp.NewClient()
+	amz.client.Logger = nil
+	amz.client.HTTPClient.Transport = &authTransport{
+		Parent: amz.client.HTTPClient.Transport,
+		Token:  token,
+	}
+	return &amz
+}
+
+func (t *authTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Set("Access-Token", t.Token)
+	return t.Parent.RoundTrip(req)
+}
+
+func (amz *AMZClient) GetPrices(list []string) (map[string]map[string]float64, error) {
+	resp, err := amz.client.Get(amzAPIURL + strings.Join(list, ","))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var prices map[string]map[string]float64
+	err = json.Unmarshal(data, &prices)
+	if err != nil {
+		return nil, err
+	}
+
+	return prices, nil
+}
