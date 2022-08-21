e53a9e2c3f716f1971e4fd70f6d68a89687159dd
Vittorio Giovara <vittorio.giovara@gmail.com>
2022-08-21 13:11:11 +0200

    tcgplayer: Add a buylist wrapper for TCG Direct without fees

---
+++ tcgplayer/direct.go
@@ -0,0 +1,74 @@
+package tcgplayer
+
+import (
+	"errors"
+	"time"
+
+	"github.com/kodabb/go-mtgban/mtgban"
+)
+
+type TCGDirectNoFees struct {
+	buylistDate     time.Time
+	buylist         mtgban.BuylistRecord
+	DirectInventory mtgban.InventoryRecord
+}
+
+func NewTCGDirectNoFees() *TCGDirectNoFees {
+	tcg := TCGDirectNoFees{}
+	tcg.buylist = mtgban.BuylistRecord{}
+	return &tcg
+}
+
+func (tcg *TCGDirectNoFees) Buylist() (mtgban.BuylistRecord, error) {
+	if len(tcg.DirectInventory) == 0 {
+		return nil, errors.New("missing inventory")
+	}
+
+	if len(tcg.buylist) > 0 {
+		return tcg.buylist, nil
+	}
+
+	for cardId, entries := range tcg.DirectInventory {
+		for _, entry := range entries {
+			directCost := 0.3 + entry.Price*(0.0895+0.025)
+
+			var replacementFees float64
+			if entry.Price < 1.50 {
+				replacementFees = entry.Price / 2
+				directCost = 0
+			} else if entry.Price < 20 {
+				replacementFees = 1.09
+			} else if entry.Price < 250 {
+				replacementFees = 3.75
+			} else {
+				replacementFees = 6.48
+			}
+
+			link := entry.URL
+			if link != "" {
+				link += "&direct=true"
+			}
+
+			buylistEntry := mtgban.BuylistEntry{
+				Conditions: entry.Conditions,
+				BuyPrice:   entry.Price - directCost - replacementFees,
+				URL:        link,
+			}
+
+			tcg.buylist[cardId] = append(tcg.buylist[cardId], buylistEntry)
+		}
+	}
+
+	tcg.buylistDate = time.Now()
+
+	return tcg.buylist, nil
+}
+
+func (tcg *TCGDirectNoFees) Info() (info mtgban.ScraperInfo) {
+	info.Name = "TCG Direct No Fees"
+	info.Shorthand = "TCGDirectNF"
+	info.BuylistTimestamp = tcg.buylistDate
+	info.MultiCondBuylist = true
+	info.NoCredit = true
+	return
+}
