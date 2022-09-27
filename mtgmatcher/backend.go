b77b23449c85413b9f64c0e89023ca415982b6cf
Vittorio Giovara <vittorio.giovara@gmail.com>
2022-09-27 20:28:58 +0200

    Fix dates for Legends and The Dark in Italian

--- mtgmatcher/backend.go
+++ mtgmatcher/backend.go
@@ -494,8 +494,8 @@ func NewDatastore(ap mtgjson.AllPrintings) {
 		}
 	}
 
-	duplicate(ap.Data, cards, uuids, "Legends Italian", "LEG", "ITA", "1995-04-01")
-	duplicate(ap.Data, cards, uuids, "The Dark Italian", "DRK", "ITA", "1995-07-01")
+	duplicate(ap.Data, cards, uuids, "Legends Italian", "LEG", "ITA", "1995-09-01")
+	duplicate(ap.Data, cards, uuids, "The Dark Italian", "DRK", "ITA", "1995-08-01")
 	duplicate(ap.Data, cards, uuids, "Chronicles Japanese", "CHR", "JPN", "1995-07-01")
 
 	// Add all names and associated uuids to the global names and hashes arrays
