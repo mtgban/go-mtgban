e4cd4d524d442cebfc422ec07f54825258e177d7
Vittorio Giovara <vittorio.giovara@gmail.com>
2023-03-30 17:56:45 -0400

    cardtrader: Rework ExportStock() to use the proper API

--- cardtrader/utils.go
+++ cardtrader/utils.go
@@ -1,89 +1,47 @@
 package cardtrader
 
 import (
-	"compress/gzip"
-	"encoding/csv"
 	"fmt"
-	"io"
-	"strconv"
 
-	"github.com/hashicorp/go-cleanhttp"
 	"github.com/mtgban/go-mtgban/mtgban"
 	"github.com/mtgban/go-mtgban/mtgmatcher"
 )
 
-const (
-	ctStockExportURL = "https://www.cardtrader.com/api/simple/v1/products/download_csv?token="
-)
-
 // Use the Simple API Token to convert your own inventory to a standard InventoryRecord
-func ExportStock(blueprints map[int]*Blueprint, token string, rates ...float64) (mtgban.InventoryRecord, error) {
-	resp, err := cleanhttp.DefaultClient().Get(ctStockExportURL + token)
+func (ct *CTAuthClient) ExportStock(blueprints map[int]*Blueprint) (mtgban.InventoryRecord, error) {
+	products, err := ct.ProductsExport()
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
 
-	gzipReader, err := gzip.NewReader(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	defer gzipReader.Close()
-
-	csvReader := csv.NewReader(gzipReader)
-	// id,blueprint_id,mkm_id,bundle,category_id,description,name_en,expansion_id,expansion_name,expansion_code,game_id,graded,price_cents,price_currency,quantity,mtg_rarity,condition,mtg_language,mtg_foil,signed,altered
-	_, err = csvReader.Read()
+	currencyRate, err := mtgban.GetExchangeRate("EUR")
 	if err != nil {
 		return nil, err
 	}
 
 	inventory := mtgban.InventoryRecord{}
-	for {
-		record, err := csvReader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			continue
-		}
-		blueprintId, err := strconv.Atoi(record[1])
-		if err != nil {
-			return nil, err
-		}
-
-		theCard, err := Preprocess(blueprints[blueprintId])
-		if err != nil {
+	for _, product := range products {
+		blueprint, found := blueprints[product.BlueprintId]
+		if !found {
 			continue
 		}
 
-		theCard.Foil, err = strconv.ParseBool(record[18])
+		theCard, err := Preprocess(blueprint)
 		if err != nil {
-			return nil, err
+			continue
 		}
+		theCard.Foil = product.Properties.Foil
 
 		cardId, err := mtgmatcher.Match(theCard)
 		if err != nil {
 			continue
 		}
 
-		priceCents, err := strconv.Atoi(record[12])
-		if err != nil {
-			return nil, err
-		}
-		price := float64(priceCents) / 100.0
+		price := currencyRate * float64(product.PriceCents) / 100.0
 
-		currency := record[13]
-		if currency == "EUR" && len(rates) > 0 && rates[0] != 0 {
-			price *= rates[0]
-		}
+		quantity := product.Quantity
 
-		quantity, err := strconv.Atoi(record[14])
-		if err != nil {
-			return nil, err
-		}
-
-		conditions := record[16]
-		conds, found := condMap[conditions]
+		condition, found := condMap[product.Properties.Condition]
 		if !found {
 			continue
 		}
@@ -91,10 +49,10 @@ func ExportStock(blueprints map[int]*Blueprint, token string, rates ...float64) 
 		inventory.AddRelaxed(cardId, &mtgban.InventoryEntry{
 			Price:      price,
 			Quantity:   quantity,
-			Conditions: conds,
+			Conditions: condition,
 			SellerName: "mtgban",
-			OriginalId: record[1],
-			InstanceId: record[0],
+			OriginalId: fmt.Sprint(product.BlueprintId),
+			InstanceId: fmt.Sprint(product.Id),
 		})
 	}
 
