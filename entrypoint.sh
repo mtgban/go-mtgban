408f6c83e0db08adaa95939f7a096486017af9f0
Jette <rpratt1337@gmail.com>
2023-06-03 13:33:33 +0000

    https://github.com/mtgban/go-mtgban

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,34 +2,33 @@
 # Cloud Run Jobs configuration script
 
 jobs=(
-    "abu '-abugames'",
-    "blueprint '-blueprint'",
-    "cardsphere '-cardsphere'",
-    "cardtrader '-cardtrader'",
-    "ck '-cardkingdom'",
-    "cksealed '-cardkingdom_sealed'",
-    "csi '-coolstuffinc'",
-    "csiofficial '-coolstuffinc_official'",
-    "hareruyabuylist '-vendors hareruya'",
-    "hareruyainv '-sellers hareruya'",
-    "jupiter '-jupitergames'",
-    "magiccorner '-magiccorner'",
-    "mkm '-mkm_index'",
-    "mkmsealed '-mkm_sealed'",
-    "mtgseattle '-mtgseattle'",
-    "mtgstocks '-mtgstocks'",
-    "mythic '-mythicmtg'",
-    "ninetyfive '-ninetyfive'",
-    "scg '-starcitygames'",
-    "strikezone '-strikezone'",
-    "tcgbuylist '-tcg_market'",
-    "tcgindex '-tcg_index'",
-    "tcgmkt '-tcg_market'",
-    "tcgsealed '-tcg_sealed'",
-    "tcgsyp '-tcgplayer_syp'",
-    "tnt '-trollandtoad'",
-    "toa '-talesofadventure'",
-    "wizcupboard '-wizardscupboard'" 
+    "abu '-abugames'"
+    "blueprint '-blueprint'"
+    "cardsphere '-cardsphere'"
+    "cardtrader '-cardtrader'"
+    "ck '-cardkingdom'"
+    "cksealed '-cardkingdom_sealed'"
+    "csi '-coolstuffinc'"
+    "csiofficial '-coolstuffinc_official'"
+    "hareruyabuylist '-vendors hareruya'"
+    "hareruyainv '-sellers hareruya'"
+    "jupiter '-jupitergames'"
+    "magiccorner '-magiccorner'"
+    "mkm '-mkm_index'"
+    "mkmsealed '-mkm_sealed'"
+    "mtgseattle '-mtgseattle'"
+    "mtgstocks '-mtgstocks'"
+    "mythic '-mythicmtg'"
+    "ninetyfive '-ninetyfive'"
+    "scg '-starcitygames'"
+    "strikezone '-strikezone'"
+    "tcgbuylist '-tcg_market'"
+    "tcgindex '-tcg_index'"
+    "tcgsealed '-tcg_sealed'"
+    "tcgsyp '-tcgplayer_syp'"
+    "tnt '-trollandtoad'"
+    "toa '-talesofadventure'"
+    "wizcupboard '-wizardscupboard'"
 )
 
 # Execute bantool binary with generated command line flags
@@ -39,6 +38,5 @@ for job in "${jobs[@]}"; do
     flags="${job#* }"
 
     export OUTPUT_PATH="gs://mtgbanzai/$job_name"
-    eval "./bantool -svc-acc '/tmp/cloudrunner' -format 'ndjson' -output'-path '$OUTPUT_PATH' $flags"
+    eval "./bantool -svc-acc '/tmp/cloudrunner' -format 'ndjson' -output-path '$OUTPUT_PATH' $flags"
 done
-
