b9bf95da0de860930571c33b8776a76748e3e625
Vittorio Giovara <vittorio.giovara@gmail.com>
2023-11-14 12:13:37 -0500

    mtgmatcher/editions: Add the companion LOTR edition of course

--- mtgmatcher/editions.go
+++ mtgmatcher/editions.go
@@ -37,7 +37,8 @@ var EditionTable = map[string]string{
 	"The Lord of the Rings Commander":              "Tales of Middle-earth Commander",
 	"LOTR - Realms and Relic Box Topper":           "Tales of Middle-earth Commander",
 
-	"The Lord of the Rings: Tales of Middle-earth Holiday Release": "The Lord of the Rings: Tales of Middle-earth",
+	"The Lord of the Rings: Tales of Middle-earth Holiday Release":            "The Lord of the Rings: Tales of Middle-earth",
+	"The Lord of the Rings: Tales of Middle-earth Holiday Release Collectors": "The Lord of the Rings: Tales of Middle-earth",
 
 	// BRR
 	"Retro Frame Artifacts":                    "The Brothers' War Retro Artifacts",
