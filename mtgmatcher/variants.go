f52da26364e21146fc7d15608cd6757bf17dcff3
Vittorio Giovara <vittorio.giovara@gmail.com>
2024-02-09 10:28:09 +0100

    mtgmatcher/variants: Add a LCI tag

--- mtgmatcher/variants.go
+++ mtgmatcher/variants.go
@@ -3631,6 +3631,7 @@ var lciVariants = map[string]map[string]string{
 		"borderless neon ink purple":             "410d",
 		"borderless neon ink green":              "410f",
 		"borderless neon ink red":                "410e",
+		"neon yellow":                            "410b",
 	},
 }
 
