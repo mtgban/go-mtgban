3c69e91ff07d9c278a70ac8d910139e528a10486
Vittorio Giovara <vittorio.giovara@gmail.com>
2024-04-09 01:03:44 -0400

    mtgmatcher: Ignore timeshifted keyword in PLST

--- mtgmatcher/mtgmatcher.go
+++ mtgmatcher/mtgmatcher.go
@@ -713,7 +713,7 @@ func adjustEdition(inCard *Card) {
 		edition = "Zendikar Expeditions"
 	case strings.Contains(edition, "Expeditions") && strings.Contains(edition, "Rising"):
 		edition = "Zendikar Rising Expeditions"
-	case inCard.Contains("Timeshifted") &&
+	case inCard.Contains("Timeshifted") && !inCard.isMysteryList() &&
 		(len(MatchInSet(inCard.Name, "TSB")) != 0 || len(MatchInSet(inCard.Name, "TSR")) != 0):
 		if len(MatchInSet(inCard.Name, "TSB")) != 0 {
 			edition = backend.Sets["TSB"].Name
