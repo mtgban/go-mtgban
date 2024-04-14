f8adc1cce712d7ada49ebe0256d00969ece97df8
Vittorio Giovara <vittorio.giovara@gmail.com>
2024-04-14 00:28:55 -0400

    starcitygames: Skip a token with the same name of a card

--- starcitygames/preprocess.go
+++ starcitygames/preprocess.go
@@ -66,6 +66,15 @@ func preprocess(card *SCGCardVariant, cardEdition, language string, foil bool, c
 	cardName = strings.Replace(cardName, "{", "", -1)
 	cardName = strings.Replace(cardName, "}", "", -1)
 
+	// Check tokens with the same name as certain cards (and skip them)
+	isToken := strings.HasPrefix(card.Name, "{") && strings.HasSuffix(card.Name, "}")
+	if isToken {
+		switch cardName {
+		case "Phyrexian Hydra":
+			return nil, mtgmatcher.ErrUnsupported
+		}
+	}
+
 	vars := mtgmatcher.SplitVariants(cardName)
 	cardName = vars[0]
 	if len(vars) > 1 {
