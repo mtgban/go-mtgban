78fa33065c02b380f2d8976d775ddb41f3c4adc5
Vittorio Giovara <vittorio.giovara@gmail.com>
2024-04-19 13:24:50 -0700

    mtgmatcher: Further simplify timeshift check for just TSB and TSR

--- mtgmatcher/mtgmatcher.go
+++ mtgmatcher/mtgmatcher.go
@@ -756,8 +756,7 @@ func adjustEdition(inCard *Card) {
 		edition = "Zendikar Expeditions"
 	case strings.Contains(edition, "Expeditions") && strings.Contains(edition, "Rising"):
 		edition = "Zendikar Rising Expeditions"
-	case inCard.Contains("Timeshifted") && !inCard.isMysteryList() &&
-		(len(MatchInSet(inCard.Name, "TSB")) != 0 || len(MatchInSet(inCard.Name, "TSR")) != 0):
+	case inCard.Contains("Timeshift") && inCard.Contains("Spiral"):
 		if len(MatchInSet(inCard.Name, "TSB")) != 0 {
 			edition = backend.Sets["TSB"].Name
 		} else if len(MatchInSet(inCard.Name, "TSR")) != 0 {
