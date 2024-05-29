291f6a16ca05f23e36266f3a2c8a146fcfead580
Vittorio Giovara <vittorio.giovara@gmail.com>
2024-05-30 01:02:06 +0200

    cardtrader/sealed: Switch to using the ids provided in mtgjson

--- cardtrader/sealed.go
+++ cardtrader/sealed.go
@@ -2,13 +2,13 @@ package cardtrader
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
 
 	"github.com/mtgban/go-mtgban/mtgban"
 	"github.com/mtgban/go-mtgban/mtgmatcher"
-	"golang.org/x/exp/slices"
 )
 
 type CardtraderSealed struct {
@@ -24,8 +24,6 @@ type CardtraderSealed struct {
 
 	inventoryDate time.Time
 	inventory     mtgban.InventoryRecord
-
-	blueprints map[int]*Blueprint
 }
 
 func NewScraperSealed(token string) (*CardtraderSealed, error) {
@@ -49,111 +47,106 @@ func (ct *CardtraderSealed) printf(format string, a ...interface{}) {
 	}
 }
 
-func (ct *CardtraderSealed) processSealedProducts(channel chan<- resultChan, uuid string, products []Product, shareCode string, rate float64) error {
-	for _, product := range products {
-		productId := uuid
-
-		if product.Properties.Language != "en" {
-			continue
-		}
-
-		switch {
-		case product.Quantity < 1,
-			product.OnVacation,
-			product.Properties.Altered:
-			continue
-		case mtgmatcher.Contains(product.Description, "ita"),
-			mtgmatcher.Contains(product.Description, "empty box"),
-			mtgmatcher.Contains(product.Description, "deck box only"):
-			continue
-		}
-
-		if product.Properties.Foil {
-			blueprint, found := ct.blueprints[product.BlueprintId]
-			if !found {
-				continue
-			}
-
-			if strings.Contains(blueprint.Name, "Secret Lair") {
-				var err error
-				productId, err = preprocessSealed("SLD", blueprint.Name+" Foil Edition")
-				if err != nil {
-					ct.printf("No association for '%s'", blueprint.Name)
-					continue
-				}
-			}
-		}
-
-		qty := product.Quantity
-		if product.Bundle {
-			qty *= 4
-		}
-
-		link := "https://www.cardtrader.com/cards/" + fmt.Sprint(product.BlueprintId)
-		if shareCode != "" {
-			link += "?share_code=" + shareCode
-		}
-
-		price := float64(product.Price.Cents) / 100
-		if product.Price.Currency != "USD" {
-			price *= rate
-		}
-
-		channel <- resultChan{
-			cardId: productId,
-			invEntry: &mtgban.InventoryEntry{
-				Conditions: "NM",
-				Price:      price,
-				Quantity:   qty,
-				URL:        link,
-				SellerName: product.User.Name,
-				Bundle:     product.User.SealedZero,
-				OriginalId: fmt.Sprint(product.BlueprintId),
-				InstanceId: fmt.Sprint(product.Id),
-			},
-		}
-	}
-
-	return nil
-}
-
-func (ct *CardtraderSealed) processEntry(channel chan<- resultChan, expansionId int, expansionName string) error {
+func (ct *CardtraderSealed) processEntry(channel chan<- resultChan, expansionId int, expansionName string, productMap map[int][]string) error {
 	allProducts, err := ct.client.ProductsForExpansion(expansionId)
 	if err != nil {
 		return err
 	}
 
-	var warned []string
-
-	for id, products := range allProducts {
-		blueprint, found := ct.blueprints[id]
-		if !found {
-			continue
-		}
-
-		uuid, err := preprocessSealed(expansionName, blueprint.Name)
-		if err != nil {
-			if err.Error() == "unknown edition" {
-				return fmt.Errorf("%s: %s", expansionName, err.Error())
-			}
-			if err.Error() == "unsupported" {
+	for _, products := range allProducts {
+		for _, product := range products {
+			uuids, found := productMap[product.BlueprintId]
+			if !found {
 				continue
 			}
-			if slices.Contains(warned, blueprint.Name) {
+
+			if product.Properties.Language != "en" {
 				continue
 			}
-			warned = append(warned, blueprint.Name)
-			ct.printf("No association for '%s' in '%s'", blueprint.Name, expansionName)
-			continue
-		}
 
-		ct.processSealedProducts(channel, uuid, products, ct.ShareCode, ct.exchangeRate)
+			uuid := uuids[0]
+			if product.Properties.Foil && len(uuids) > 1 {
+				uuid = uuids[1]
+			}
+
+			switch {
+			case product.Quantity < 1,
+				product.OnVacation,
+				product.Properties.Altered:
+				continue
+			case mtgmatcher.Contains(product.Description, "ita"),
+				mtgmatcher.Contains(product.Description, "empty box"),
+				mtgmatcher.Contains(product.Description, "deck box only"):
+				continue
+			}
+
+			qty := product.Quantity
+			if product.Bundle {
+				qty *= 4
+			}
+
+			link := "https://www.cardtrader.com/cards/" + fmt.Sprint(product.BlueprintId)
+			if ct.ShareCode != "" {
+				link += "?share_code=" + ct.ShareCode
+			}
+
+			price := float64(product.Price.Cents) / 100
+			if product.Price.Currency != "USD" {
+				price *= ct.exchangeRate
+			}
+
+			channel <- resultChan{
+				cardId: uuid,
+				invEntry: &mtgban.InventoryEntry{
+					Conditions: "NM",
+					Price:      price,
+					Quantity:   qty,
+					URL:        link,
+					SellerName: product.User.Name,
+					Bundle:     product.User.SealedZero,
+					OriginalId: fmt.Sprint(product.BlueprintId),
+					InstanceId: fmt.Sprint(product.Id),
+				},
+			}
+		}
 	}
 
 	return nil
 }
 
 func (ct *CardtraderSealed) scrape() error {
+	productMap := map[int][]string{}
+	for _, uuid := range mtgmatcher.GetSealedUUIDs() {
+		co, err := mtgmatcher.GetUUID(uuid)
+		if err != nil || !co.Sealed {
+			continue
+		}
+		ctId := co.Identifiers["cardtraderId"]
+
+		// Some products do not carry an id because they are already assigned
+		// For specific cases, look for them since we have the canonical number
+		if ctId == "" && co.SetCode == "SLD" && strings.HasSuffix(co.Name, " Foil") {
+			uuids, err := mtgmatcher.SearchSealedEquals(strings.TrimSuffix(co.Name, " Foil"))
+			if err != nil {
+				continue
+			}
+			subco, err := mtgmatcher.GetUUID(uuids[0])
+			if err != nil {
+				continue
+			}
+			ctId = subco.Identifiers["cardtraderId"]
+		}
+		cardtraderId, err := strconv.Atoi(ctId)
+		if err != nil {
+			continue
+		}
+		// We also know that nonfoil comes before foil since product names are sorted
+		// so we can guarantee that the first element is nonfoil, and the second one
+		// is actually foil
+		productMap[cardtraderId] = append(productMap[cardtraderId], uuid)
+	}
+	ct.printf("Loaded %d sealed products", len(productMap))
+
 	expansionsRaw, err := ct.client.Expansions()
 	if err != nil {
 		return err
@@ -182,8 +175,7 @@ func (ct *CardtraderSealed) scrape() error {
 	}
 	ct.printf("Found %d blueprints", len(blueprintsRaw))
 
-	blueprints, expansions := FormatBlueprints(blueprintsRaw, expansionsRaw, true)
-	ct.blueprints = blueprints
+	_, expansions := FormatBlueprints(blueprintsRaw, expansionsRaw, true)
 	ct.printf("Parsing %d expansions", len(expansions))
 
 	expansionIds := make(chan int)
@@ -194,7 +186,7 @@ func (ct *CardtraderSealed) scrape() error {
 		wg.Add(1)
 		go func() {
 			for expansionId := range expansionIds {
-				err := ct.processEntry(results, expansionId, expansions[expansionId])
+				err := ct.processEntry(results, expansionId, expansions[expansionId], productMap)
 				if err != nil {
 					ct.printf("%v", err)
 				}
