279c61989dfe9fac002d05062967eeda6dc2810d
Vittorio Giovara <vittorio.giovara@gmail.com>
2024-07-03 16:06:54 +0200

    toamagic: Drop buylist module

--- toamagic/toamagic.go
+++ toamagic/toamagic.go
@@ -18,10 +18,6 @@ const (
 	defaultConcurrency = 8
 
 	inventoryURL = "https://www.toamagic.com/catalog/magic_singles/8?layout=false"
-	buylistURL   = "https://www.toamagic.com/buylist/magic_singles/8?layout=false"
-
-	modeInventory = "inventory"
-	modeBuylist   = "buylist"
 )
 
 type TOAMagic struct {
@@ -29,10 +25,7 @@ type TOAMagic struct {
 	MaxConcurrency int
 
 	inventoryDate time.Time
-	buylistDate   time.Time
-
-	inventory mtgban.InventoryRecord
-	buylist   mtgban.BuylistRecord
+	inventory     mtgban.InventoryRecord
 
 	client *retryablehttp.Client
 }
@@ -40,7 +33,6 @@ type TOAMagic struct {
 func NewScraper() *TOAMagic {
 	toa := TOAMagic{}
 	toa.inventory = mtgban.InventoryRecord{}
-	toa.buylist = mtgban.BuylistRecord{}
 	toa.MaxConcurrency = defaultConcurrency
 	toa.client = retryablehttp.NewClient()
 	toa.client.Logger = nil
@@ -50,7 +42,6 @@ func NewScraper() *TOAMagic {
 type responseChan struct {
 	cardId   string
 	invEntry *mtgban.InventoryEntry
-	buyEntry *mtgban.BuylistEntry
 }
 
 func (toa *TOAMagic) printf(format string, a ...interface{}) {
@@ -59,7 +50,7 @@ func (toa *TOAMagic) printf(format string, a ...interface{}) {
 	}
 }
 
-func (toa *TOAMagic) processProduct(channel chan<- responseChan, productPath, mode string) error {
+func (toa *TOAMagic) processProduct(channel chan<- responseChan, productPath string) error {
 	resp, err := toa.client.Get("https://www.toamagic.com" + productPath + "?layout=false&filter_by_stock=in-stock")
 	if err != nil {
 		return err
@@ -90,7 +81,7 @@ func (toa *TOAMagic) processProduct(channel chan<- responseChan, productPath, mo
 		})
 
 		for _, link := range links {
-			err := toa.processProduct(channel, link, mode)
+			err := toa.processProduct(channel, link)
 			if err != nil {
 				toa.printf("%s", err.Error())
 			}
@@ -99,9 +90,6 @@ func (toa *TOAMagic) processProduct(channel chan<- responseChan, productPath, mo
 	}
 
 	xpath := `ul[class="products"] li[class="product"] div[class="inner"] div[class="meta"]`
-	if mode == modeBuylist {
-		xpath = `ul[class="products"] li[class="product"] div[class="inner"] div[class="meta credit"]`
-	}
 	doc.Find(xpath).Each(func(_ int, s *goquery.Selection) {
 		link, _ := s.Find("a").Attr("href")
 
@@ -114,9 +102,6 @@ func (toa *TOAMagic) processProduct(channel chan<- responseChan, productPath, mo
 		}
 
 		container := `div[class="list-variants grid small-12 medium-8"] div[class="variant-row in-stock"] span[class="variant-main-info small-12 medium-4 large-5 column eat-both"]`
-		if mode == modeBuylist {
-			container = `span[class="variant-main-info small-12 medium-5 large-5 column eat-both"]`
-		}
 		// This will skip the variants in the search page
 		condLang := s.Find(container + ` span[class="variant-short-info variant-description"]`).Text()
 
@@ -137,31 +122,24 @@ func (toa *TOAMagic) processProduct(channel chan<- responseChan, productPath, mo
 		}
 
 		conditions := ""
-		if mode == modeInventory {
-			cond := strings.Split(condLang, ", ")[0]
-			cond = strings.TrimPrefix(cond, "Website Exclusive ")
-			switch cond {
-			case "NM-Mint", "NM":
-				conditions = "NM"
-			case "Light Play", "LP":
-				conditions = "SP"
-			case "Moderate Play":
-				conditions = "MP"
-			case "Heavy Play":
-				conditions = "HP"
-			case "Damaged":
-				conditions = "PO"
-			case "Graded":
-				return
-			default:
-				toa.printf("Unsupported %s condition for %s", cond, title)
-				return
-			}
-		} else if mode == modeBuylist {
-			// Early exit to avoid catching sealed and similar
-			if condLang != "NM-Mint, English" {
-				return
-			}
+		cond := strings.Split(condLang, ", ")[0]
+		cond = strings.TrimPrefix(cond, "Website Exclusive ")
+		switch cond {
+		case "NM-Mint", "NM":
+			conditions = "NM"
+		case "Light Play", "LP":
+			conditions = "SP"
+		case "Moderate Play":
+			conditions = "MP"
+		case "Heavy Play":
+			conditions = "HP"
+		case "Damaged":
+			conditions = "PO"
+		case "Graded":
+			return
+		default:
+			toa.printf("Unsupported %s condition for %s", cond, title)
+			return
 		}
 
 		theCard, err := preprocess(cardName, edition, variant)
@@ -203,49 +181,16 @@ func (toa *TOAMagic) processProduct(channel chan<- responseChan, productPath, mo
 			return
 		}
 
-		if mode == modeInventory {
-			out := responseChan{
-				cardId: cardId,
-				invEntry: &mtgban.InventoryEntry{
-					Price:      price,
-					Conditions: conditions,
-					Quantity:   qty,
-					URL:        "https://www.toamagic.com" + link,
-				},
-			}
-			channel <- out
-		} else if mode == modeBuylist {
-			var priceRatio, sellPrice float64
-
-			invCards := toa.inventory[cardId]
-			for _, invCard := range invCards {
-				sellPrice = invCard.Price
-				break
-			}
-			if sellPrice > 0 {
-				priceRatio = price / sellPrice * 100
-			}
-
-			deductions := []float64{1, 1, 0.5, 0.3}
-			for i, deduction := range deductions {
-				var quantity int
-				if i == 0 {
-					quantity = qty
-				}
-
-				out := responseChan{
-					cardId: cardId,
-					buyEntry: &mtgban.BuylistEntry{
-						Conditions: mtgban.DefaultGradeTags[i],
-						BuyPrice:   price * deduction,
-						PriceRatio: priceRatio,
-						Quantity:   quantity,
-						URL:        "https://www.toamagic.com" + link,
-					},
-				}
-				channel <- out
-			}
+		out := responseChan{
+			cardId: cardId,
+			invEntry: &mtgban.InventoryEntry{
+				Price:      price,
+				Conditions: conditions,
+				Quantity:   qty,
+				URL:        "https://www.toamagic.com" + link,
+			},
 		}
+		channel <- out
 	})
 
 	// Search for the next page, if not found we processed them all
@@ -254,14 +199,11 @@ func (toa *TOAMagic) processProduct(channel chan<- responseChan, productPath, mo
 		return nil
 	}
 
-	return toa.processProduct(channel, next, mode)
+	return toa.processProduct(channel, next)
 }
 
-func (toa *TOAMagic) scrape(mode string) error {
+func (toa *TOAMagic) scrape() error {
 	link := inventoryURL
-	if mode == modeBuylist {
-		link = buylistURL
-	}
 	resp, err := toa.client.Get(link)
 	if err != nil {
 		return err
@@ -293,7 +235,7 @@ func (toa *TOAMagic) scrape(mode string) error {
 		wg.Add(1)
 		go func() {
 			for productPath := range products {
-				err := toa.processProduct(results, productPath, mode)
+				err := toa.processProduct(results, productPath)
 				if err != nil {
 					toa.printf("%v", err)
 				}
@@ -317,19 +259,13 @@ func (toa *TOAMagic) scrape(mode string) error {
 		var err error
 		if record.invEntry != nil {
 			err = toa.inventory.AddRelaxed(record.cardId, record.invEntry)
-		} else if record.buyEntry != nil {
-			err = toa.buylist.Add(record.cardId, record.buyEntry)
 		}
 		if err != nil {
 			toa.printf("%s", err.Error())
 		}
 	}
 
-	if mode == modeInventory {
-		toa.inventoryDate = time.Now()
-	} else if mode == modeBuylist {
-		toa.buylistDate = time.Now()
-	}
+	toa.inventoryDate = time.Now()
 
 	return nil
 }
@@ -339,7 +275,7 @@ func (toa *TOAMagic) Inventory() (mtgban.InventoryRecord, error) {
 		return toa.inventory, nil
 	}
 
-	err := toa.scrape(modeInventory)
+	err := toa.scrape()
 	if err != nil {
 		return nil, err
 	}
@@ -347,23 +283,9 @@ func (toa *TOAMagic) Inventory() (mtgban.InventoryRecord, error) {
 	return toa.inventory, nil
 }
 
-func (toa *TOAMagic) Buylist() (mtgban.BuylistRecord, error) {
-	if len(toa.buylist) > 0 {
-		return toa.buylist, nil
-	}
-
-	err := toa.scrape(modeBuylist)
-	if err != nil {
-		return nil, err
-	}
-
-	return toa.buylist, nil
-}
-
 func (toa *TOAMagic) Info() (info mtgban.ScraperInfo) {
 	info.Name = "Tales of Adventure"
 	info.Shorthand = "TOA"
 	info.InventoryTimestamp = &toa.inventoryDate
-	info.BuylistTimestamp = &toa.buylistDate
 	return
 }
