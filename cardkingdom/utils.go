cd6e5e4cd175b063a5df18eb6ec41af2a288288c
Vittorio Giovara <vittorio.giovara@gmail.com>
2024-09-09 02:50:33 +0200

    cardkingdom: Return the full response after cart operations

--- cardkingdom/utils.go
+++ cardkingdom/utils.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/cookiejar"
+	"time"
 
 	"github.com/hashicorp/go-cleanhttp"
 )
@@ -31,6 +33,78 @@ type CartRequest struct {
 	Quantity  int    `json:"quantity"`
 }
 
+type CartResponse struct {
+	AutoValidateCart  bool      `json:"auto_validate_cart"`
+	ForceValidateCart bool      `json:"force_validate_cart"`
+	HasPriceChange    bool      `json:"has_price_change"`
+	HasQuantityChange bool      `json:"has_quantity_change"`
+	ID                int       `json:"id"`
+	ItemCount         int       `json:"item_count"`
+	LastAccess        time.Time `json:"last_access"`
+	LineitemCount     int       `json:"lineitem_count"`
+	Lineitems         []struct {
+		Product struct {
+			Model           string `json:"model"`
+			Width           any    `json:"width"`
+			Height          any    `json:"height"`
+			Depth           any    `json:"depth"`
+			Weight          int    `json:"weight"`
+			IsShiny         bool   `json:"is_shiny"`
+			ProductSlug     string `json:"product_slug"`
+			CategorySlug    string `json:"category_slug"`
+			URI             string `json:"uri"`
+			MaxQtyAvailable int    `json:"max_qty_available"`
+			PriceBuy        string `json:"price_buy"`
+			PriceSale       any    `json:"price_sale"`
+			OrderLimit      any    `json:"order_limit"`
+			BuyLimit        int    `json:"buy_limit"`
+			BorderClass     string `json:"border_class"`
+			Rarity          string `json:"rarity"`
+			ShortName       string `json:"short_name"`
+			IsActive        bool   `json:"is_active"`
+		} `json:"product"`
+		Title                      string `json:"title"`
+		Edition                    string `json:"edition"`
+		Rarity                     string `json:"rarity"`
+		Variation                  string `json:"variation"`
+		Weight                     int    `json:"weight"`
+		AllowInternationalShipping bool   `json:"allow_international_shipping"`
+		ID                         int    `json:"id"`
+		IsPresale                  bool   `json:"is_presale"`
+		ShipDate                   string `json:"ship_date"`
+		IsShiny                    bool   `json:"is_shiny"`
+		DefaultImage               string `json:"default_image"`
+		Images                     []struct {
+			URI             string `json:"uri"`
+			ImgixURI        any    `json:"imgix_uri"`
+			IsDefault       bool   `json:"is_default"`
+			ImgixAttributes struct {
+				BlendMode string `json:"blend-mode"`
+				Blend     string `json:"blend"`
+			} `json:"imgix_attributes"`
+		} `json:"images"`
+		Name             string `json:"name"`
+		Price            string `json:"price"`
+		PriceAfterCoupon string `json:"price_after_coupon"`
+		OriginalPrice    string `json:"original_price"`
+		ProductID        int    `json:"product_id"`
+		Style            string `json:"style"`
+		Total            string `json:"total"`
+		Qty              int    `json:"qty"`
+		OriginalQuantity int    `json:"original_quantity"`
+		IsBuying         bool   `json:"is_buying"`
+		IsSelling        int    `json:"is_selling"`
+		IsTaxable        int    `json:"is_taxable"`
+		CouponDiscount   string `json:"coupon_discount"`
+	} `json:"lineitems"`
+	PremiumOffer               string `json:"premium_offer"`
+	Status                     string `json:"status"`
+	Subtotal                   string `json:"subtotal"`
+	SubtotalStorecredit        string `json:"subtotal_storecredit"`
+	Type                       string `json:"type"`
+	NeedsCartMergeNotification bool   `json:"needs_cart_merge_notification"`
+}
+
 var condMap = map[string]string{
 	"NM": "NM",
 	"SP": "EX",
@@ -43,15 +117,15 @@ const (
 	ckBuylistAddURL   = "https://www.cardkingdom.com/api/sellcart/add"
 )
 
-func (ck *CookieClient) SetCartInventory(ckId, cond string, qty int) (string, error) {
+func (ck *CookieClient) SetCartInventory(ckId, cond string, qty int) (*CartResponse, error) {
 	return ck.setCart(ckInventoryAddURL, ckId, cond, qty)
 }
 
-func (ck *CookieClient) SetCartBuylist(ckId string, qty int) (string, error) {
+func (ck *CookieClient) SetCartBuylist(ckId string, qty int) (*CartResponse, error) {
 	return ck.setCart(ckBuylistAddURL, ckId, "NM", qty)
 }
 
-func (ck *CookieClient) setCart(link, ckId, cond string, qty int) (string, error) {
+func (ck *CookieClient) setCart(link, ckId, cond string, qty int) (*CartResponse, error) {
 	style, found := condMap[cond]
 	if found {
 		cond = style
@@ -65,27 +139,31 @@ func (ck *CookieClient) setCart(link, ckId, cond string, qty int) (string, error
 
 	reqBody, err := json.Marshal(&payload)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	resp, err := ck.Post(link, "application/json", bytes.NewReader(reqBody))
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer resp.Body.Close()
-
-	// Nothing interesting in the response
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	response := string(data)
 
 	if resp.StatusCode != http.StatusOK {
 		err = errors.New("cart not ok")
 	}
 
-	return response, err
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var cartResponse CartResponse
+	err = json.Unmarshal(data, &cartResponse)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshal error, got: %s", string(data))
+	}
+
+	return &cartResponse, nil
 }
 
 func (ck *CookieClient) Get(link string) (*http.Response, error) {
