9b22ee08fc8516ac60806692ff33eec8ee7dd7dd
Vittorio Giovara <vittorio.giovara@gmail.com>
2025-06-27 01:37:35 -0400

    manapool: Add sealed scraper

--- manapool/api.go
+++ manapool/api.go
@@ -26,8 +26,20 @@ type Card struct {
 	PriceCentsNmFoil     int    `json:"price_cents_nm_foil"`
 }
 
+type Product struct {
+	ProductType        string `json:"product_type"`
+	ProductID          string `json:"product_id"`
+	SetCode            string `json:"set_code"`
+	Name               string `json:"name"`
+	TcgplayerProductID int    `json:"tcgplayer_product_id"`
+	LanguageID         string `json:"language_id"`
+	LowPrice           int    `json:"low_price"`
+	AvailableQuantity  int    `json:"available_quantity"`
+}
+
 const (
 	manapoolURL = "https://manapool.com/api/beta/pricelists/cards.json"
+	sealedURL   = "https://manapool.com/api/v1/prices/sealed"
 )
 
 type Client struct {
@@ -62,6 +74,37 @@ func (mp *Client) GetPriceList() ([]Card, error) {
 			AsOf time.Time `json:"as_of"`
 		} `json:"meta"`
 		Data []Card `json:"data"`
+	}
+	err = json.Unmarshal(data, &pricelist)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshal error for list, got: %s", string(data))
+	}
+
+	return pricelist.Data, nil
+}
+
+func (mp *Client) GetSealedList() ([]Product, error) {
+	req, err := http.NewRequest("GET", sealedURL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := mp.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var pricelist struct {
+		Meta struct {
+			AsOf time.Time `json:"as_of"`
+		} `json:"meta"`
+		Data []Product `json:"data"`
 	}
 	err = json.Unmarshal(data, &pricelist)
 	if err != nil {
---
+++ manapool/sealed.go
@@ -0,0 +1,115 @@
+package manapool
+
+import (
+	"net/url"
+	"strconv"
+	"time"
+
+	"github.com/mtgban/go-mtgban/mtgban"
+	"github.com/mtgban/go-mtgban/mtgmatcher"
+)
+
+type ManapoolSealed struct {
+	LogCallback mtgban.LogCallbackFunc
+	Partner     string
+
+	inventoryDate time.Time
+	inventory     mtgban.InventoryRecord
+}
+
+func NewScraperSealed() *ManapoolSealed {
+	mp := ManapoolSealed{}
+	mp.inventory = mtgban.InventoryRecord{}
+	return &mp
+}
+
+func (mp *ManapoolSealed) printf(format string, a ...interface{}) {
+	if mp.LogCallback != nil {
+		mp.LogCallback("[MPSealed] "+format, a...)
+	}
+}
+
+func (mp *ManapoolSealed) scrape() error {
+	mpClient := NewClient()
+	pricelist, err := mpClient.GetSealedList()
+	if err != nil {
+		return err
+	}
+
+	mp.printf("Found %d prices", len(pricelist))
+
+	u, _ := url.Parse("https://manapool.com/browse_sealed")
+	v := url.Values{}
+	if mp.Partner != "" {
+		v.Set("ref", mp.Partner)
+	}
+	u.RawQuery = v.Encode()
+
+	var foundProduct int
+
+	sets := mtgmatcher.GetAllSets()
+	for _, code := range sets {
+		set, _ := mtgmatcher.GetSet(code)
+
+		// Skip products without Sealed or Booster information
+		switch set.Code {
+		case "FBB", "4BB", "DRKITA", "LEGITA", "RIN", "4EDALT", "BCHR":
+			continue
+		}
+
+		for _, product := range set.SealedProduct {
+			tcgIdStr, found := product.Identifiers["tcgplayerProductId"]
+			if !found {
+				continue
+			}
+
+			tcgId, err := strconv.Atoi(tcgIdStr)
+			if err != nil {
+				continue
+			}
+
+			for _, sealed := range pricelist {
+				if tcgId != sealed.TcgplayerProductID {
+					continue
+				}
+
+				foundProduct++
+
+				out := &mtgban.InventoryEntry{
+					Price:    float64(sealed.LowPrice) / 100.0,
+					URL:      u.String(),
+					Quantity: sealed.AvailableQuantity,
+				}
+				err = mp.inventory.AddUnique(product.UUID, out)
+			}
+		}
+	}
+
+	perc := float64(foundProduct) * 100 / float64(len(pricelist))
+	mp.printf("Found %d products over %d items (%.02f%%)", foundProduct, len(pricelist), perc)
+
+	mp.inventoryDate = time.Now()
+
+	return nil
+}
+
+func (mp *ManapoolSealed) Inventory() (mtgban.InventoryRecord, error) {
+	if len(mp.inventory) > 0 {
+		return mp.inventory, nil
+	}
+
+	err := mp.scrape()
+	if err != nil {
+		return nil, err
+	}
+
+	return mp.inventory, nil
+}
+
+func (mp *ManapoolSealed) Info() (info mtgban.ScraperInfo) {
+	info.Name = "Manapool"
+	info.Shorthand = "MPSaled"
+	info.InventoryTimestamp = &mp.inventoryDate
+	info.SealedMode = true
+	return
+}
