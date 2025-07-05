mtgban/go-mtgban

commit cbc6a9804523c239c9b3bc372534b2f58974dd33

mtgmatcher: Move number precheck to a separate function
Improves readability. Drop the AttractionLights check for something
simpler (checking the edition directly).

mtgmatcher/card.go
@@ -906,3 +906,30 @@ func ParseCommanderEdition(edition, variant string) string {
 
 	return ""
 }
+
+// Check if the card number (if present) is reliable
+func (c *InputCard) shouldIgnoreNumber(setName, num string) bool {
+	// No misprints or WCD
+	if c.Contains("Misprint") || c.isWorldChamp() {
+		return true
+	}
+
+	// This is better handled in thelistCheck()
+	if c.isMysteryList() && !c.Contains("Unfinity") {
+		return true
+	}
+
+	// Unfinity numbers could refer to Attractions
+	if Contains(c.Edition, "unf") && (strings.Contains(c.Variation, "/") || strings.Contains(c.Variation, "-")) {
+		return true
+	}
+
+	// If the number is the same as in the edition, there might be
+	// variation pollution, therefore unreliable (unless they are years)
+	if num != "" && strings.Contains(setName, num) && ExtractYear(setName) == "" {
+		return true
+	}
+
+	return false
+
+}

mtgmatcher/filter.go
@@ -793,14 +793,7 @@ func filterCards(inCard *InputCard, cardSet map[string][]Card) (outCards []Card)
 			if num == "" && card.SetCode == "SLD" {
 				num = ExtractNumberAny(inCard.Variation)
 			}
-			if inCard.Contains("Misprint") ||
-				inCard.isWorldChamp() ||
-				(inCard.isMysteryList() && !inCard.Contains("Unfinity")) || // this is better handled in thelistCheck()
-				(card.AttractionLights != nil && (strings.Contains(inCard.Variation, "/") || strings.Contains(inCard.Variation, "-"))) ||
-				// If the number is the same as in the edition, it might be a
-				// variation pollutin, so it is unreliable - ignore sets with years
-				// as it may trigger false postives
-				(num != "" && ExtractYear(set.Name) == "" && strings.Contains(set.Name, num)) {
+			if inCard.shouldIgnoreNumber(set.Name, num) {
 				checkNum = false
 				logger.Println("Skipping number check")
 			}

commit e021c420787b35f2339071c03d5989ea1291f226

mtgmatcher: Fix parsing UNF cards with numbers
Restores behavior broken in cbc6a9804523c239c9b3bc372534b2f58974dd33.

mtgmatcher/api.go
@@ -286,6 +286,15 @@ func hasPrinting(name, field, value string, editions ...string) bool {
 		checkFunc = func(card Card, value string) bool {
 			return card.HasFinish(value)
 		}
+	case "field":
+		switch value {
+		case "attractionLights":
+			checkFunc = func(card Card, value string) bool {
+				return card.AttractionLights != nil
+			}
+		default:
+			return false
+		}
 	default:
 		return false
 	}

mtgmatcher/card.go
@@ -920,8 +920,10 @@ func (c *InputCard) shouldIgnoreNumber(setName, num string) bool {
 	}
 
 	// Unfinity numbers could refer to Attractions
-	if Contains(c.Edition, "unf") && (strings.Contains(c.Variation, "/") || strings.Contains(c.Variation, "-")) {
-		return true
+	if Contains(c.Edition, "unf") {
+		if hasPrinting(c.Name, "field", "attractionLights", "UNF") && (strings.Contains(c.Variation, "/") || strings.Contains(c.Variation, "-")) {
+			return true
+		}
 	}
 
 	// If the number is the same as in the edition, there might be