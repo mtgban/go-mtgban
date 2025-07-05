9205c8ab0309807e73fc928d309530c2b17e3223
Vittorio Giovara <vittorio.giovara@gmail.com>
2025-07-05 20:49:45 +0200

    cardmarket: Add a function to adjust product names in the guide

--- cardmarket/utils.go
+++ cardmarket/utils.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/hashicorp/go-cleanhttp"
 )
@@ -139,4 +140,46 @@ func getProductList(gameId int, link string) ([]ProductList, error) {
 	}
 
 	return response.Products, nil
+}
+
+// Make sure there are no duplicate names within the same edition
+func SanitizeProductList(productList []ProductList) {
+	// Lower product id means lower version number
+	for i := range productList {
+		name := productList[i].Name
+		// Skip already processed entries
+		if strings.Contains(name, "(V.") {
+			continue
+		}
+
+		version := 0
+		first := 0
+		for j := range productList {
+			// Look through the current edition only
+			if productList[i].ExpansionID != productList[j].ExpansionID {
+				continue
+			}
+
+			if name == productList[j].Name {
+				// Save the reference to the first element as it's not guaranteed that
+				// a. we'll find duplicates in the same edition
+				// b. duplicates are grouped together (they might have wide gaps
+				// At least the rule of lower id -> lower version number still stands
+				if version == 0 {
+					first = j
+				}
+				version++
+
+				// If multiple ids are found, we need to update the version of the first
+				// element (and only the first time) and then update the version of the
+				// current entry
+				if version > 1 {
+					if version == 2 {
+						productList[first].Name = fmt.Sprintf("%s (V.%d)", name, 1)
+					}
+					productList[j].Name = fmt.Sprintf("%s (V.%d)", name, version)
+				}
+			}
+		}
+	}
 }
