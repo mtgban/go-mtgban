mtgban/go-mtgban

commit d48605578ca97cecc91f86d0be8fff27b93d30dd

cardmarket: Simplify managing the retrayable client

cardmarket/api.go
@@ -14,7 +14,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	retryablehttp "github.com/hashicorp/go-retryablehttp"
+	"github.com/hashicorp/go-retryablehttp"
 )
 
 const (
@@ -30,29 +30,30 @@ const (
 )
 
 type MKMClient struct {
-	client *retryablehttp.Client
+	client *http.Client
 }
 
 func NewMKMClient(appToken, appSecret string) *MKMClient {
 	mkm := MKMClient{}
-	mkm.client = retryablehttp.NewClient()
-	mkm.client.Logger = nil
+	client := retryablehttp.NewClient()
+	client.Logger = nil
 	// The api is very sensitive to multiple concurrent requests,
 	// This backoff strategy lets the system chill out a bit before retrying
-	mkm.client.Backoff = retryablehttp.LinearJitterBackoff
-	mkm.client.RetryWaitMin = 2 * time.Second
-	mkm.client.RetryWaitMax = 10 * time.Second
-	mkm.client.RetryMax = 20
-	mkm.client.HTTPClient.Transport = &authTransport{
-		Parent:    mkm.client.HTTPClient.Transport,
+	client.Backoff = retryablehttp.LinearJitterBackoff
+	client.RetryWaitMin = 2 * time.Second
+	client.RetryWaitMax = 10 * time.Second
+	client.RetryMax = 20
+	client.HTTPClient.Transport = &authTransport{
+		Parent:    client.HTTPClient.Transport,
 		AppToken:  appToken,
 		AppSecret: appSecret,
 	}
+	mkm.client = client.StandardClient()
 	return &mkm
 }
 
 func (mkm *MKMClient) RequestNo() int {
-	return mkm.client.HTTPClient.Transport.(*authTransport).RequestNo
+	return mkm.client.Transport.(*authTransport).RequestNo
 }
 
 type MKMExpansion struct {

commit cd3ee7347abeeea0531329396536f5b5f7321336

cardmarket/api: Fix type assertion
Introduced in d48605578ca97cecc91f86d0be8fff27b93d30dd.

cardmarket/api.go
@@ -32,6 +32,7 @@ const (
 
 type MKMClient struct {
 	client *http.Client
+	auth   *authTransport
 }
 
 func NewMKMClient(appToken, appSecret string) *MKMClient {
@@ -44,17 +45,21 @@ func NewMKMClient(appToken, appSecret string) *MKMClient {
 	client.RetryWaitMin = 2 * time.Second
 	client.RetryWaitMax = 10 * time.Second
 	client.RetryMax = 20
-	client.HTTPClient.Transport = &authTransport{
+
+	auth := &authTransport{
 		Parent:    client.HTTPClient.Transport,
 		AppToken:  appToken,
 		AppSecret: appSecret,
 	}
+
+	client.HTTPClient.Transport = auth
+	mkm.auth = auth
 	mkm.client = client.StandardClient()
 	return &mkm
 }
 
 func (mkm *MKMClient) RequestNo() int {
-	return mkm.client.Transport.(*authTransport).RequestNo
+	return mkm.auth.RequestNo
 }
 
 type MKMExpansion struct {