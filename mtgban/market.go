61334044858abc5008d18792ae460c501f73c2f0
Vittorio Giovara <vittorio.giovara@gmail.com>
2025-09-26 03:58:20 +0200

    mtgban: Remove deprecated client

--- mtgban/market.go
+++ mtgban/market.go
@@ -1,7 +1,6 @@
 package mtgban
 
 import (
-	"context"
 	"fmt"
 	"slices"
 )
@@ -52,66 +51,4 @@ func BuylistForVendor(vendor Trader, vendorName string) (BuylistRecord, error) {
 	}
 
 	return traderpost, nil
-}
-
-// Base structure for the conversion of a Market to a standard Seller
-// This will hold the original Market scraper and retrieve the loaded
-// subseller from its ScraperInfo
-type BaseMarket struct {
-	inventory InventoryRecord
-	info      ScraperInfo
-	scraper   Market
-}
-
-func (m *BaseMarket) Load(ctx context.Context) error {
-	// Reset internal state
-	m.inventory = nil
-	return m.scraper.Load(ctx)
-}
-
-func (m *BaseMarket) Inventory() (InventoryRecord, error) {
-	if m.inventory == nil {
-		// Retrieve inventory from the original scraper
-		inventory, err := InventoryForSeller(m.scraper, m.info.Name)
-		if err != nil {
-			return nil, err
-		}
-		m.inventory = inventory
-	}
-	return m.inventory, nil
-}
-
-func (m *BaseMarket) Info() ScraperInfo {
-	return m.info
-}
-
-// Base structure for the conversion of a Trader to a standard Vendor
-// This will hold the original Trader scraper and retrieve the loaded
-// subvendor from its ScraperInfo
-type BaseTrader struct {
-	buylist BuylistRecord
-	info    ScraperInfo
-	scraper Trader
-}
-
-func (m *BaseTrader) Load(ctx context.Context) error {
-	// Reset internal state
-	m.buylist = nil
-	return m.scraper.Load(ctx)
-}
-
-func (m *BaseTrader) Buylist() (BuylistRecord, error) {
-	if m.buylist == nil {
-		// Retrieve buylist from the original scraper
-		buylist, err := BuylistForVendor(m.scraper, m.info.Name)
-		if err != nil {
-			return nil, err
-		}
-		m.buylist = buylist
-	}
-	return m.buylist, nil
-}
-
-func (m *BaseTrader) Info() ScraperInfo {
-	return m.info
 }
